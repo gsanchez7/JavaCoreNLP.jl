9175e079049f270df6074e60003f3335b3df2d06
Gabriel Sanchez <gsanchez@Gabriels-MBP.attlocal.net>
2020-04-13 13:31:27 -0700

    changing isapple to isbsd

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,5 @@
-mvn = Sys.is_apple() ? "mvn.cmd" : "mvn"
-which = Sys.is_apple() ? "where" : "which"
+mvn = Sys.isbsd() ? "mvn.cmd" : "mvn"
+which = Sys.is_bsd() ? "where" : "which"
 
 try
     run(`$which $mvn`)
